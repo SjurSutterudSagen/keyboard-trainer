60e2e1ab4cfb966f826e0e9646916962c9c88adb
Sjur Sutterud Sagen <sjur@sutterudsagen.no>
2025-08-30 19:01:49 +0200

    Add e2e test framework

---
+++ src/app/components/footer/footer.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+
+import { Footer } from "./footer";
+
+test("footer renders correctly", () => {
+    render(<Footer />);
+
+    const footer = screen.getByRole('contentinfo');
+
+   expect(footer).toBeDefined();
+   expect(footer).toHaveTextContent("Footer");
+});
